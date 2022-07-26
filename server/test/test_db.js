e2b267b8c79e536170e621cd6aaa1314086917df
ManOwnFire <b.avg@hotmail.com>
2022-07-26 14:43:39 +0300

    working on dataMultitableHaus multi test

--- server/test/test_db.js
+++ server/test/test_db.js
@@ -113,7 +113,9 @@ describe('Test DB Connectivity', ()=>{
     // persons queries tests
 
     //dictionary containg query name as key and expected values as keyvalues
-    const personQueries = {personsData: "object", contactData: "array", addresses: "array"}
+    const personQueries = {
+        personsData: "object", contactData: "array", addresses: "array", arb_grp: "array", bezugspersonen: "array", bezugskinder: "array"
+    }
 
     for (var q in personQueries){
         describe(`GET /${q}`, () => {
@@ -129,4 +131,105 @@ describe('Test DB Connectivity', ()=>{
             })
         })
     }
+
+    describe("GET /personsRecords", () => {
+        it("should get all records of a Person in the db.", (done)=>{
+            chai.request(server)
+                .get("/personsRecords")
+                .query({table: "person_haushalt", person_id: "72", sortByColumn: "person_id"})
+                .end((err, response) => {
+                    response.should.have.status(200);
+                    response.body.should.be.a('array');
+                done();
+                })
+        })
+    })
+
+    // simple queries
+    const simpleQueries = ["hausList", "agList", "lerngruppenList"]
+
+    simpleQueries.forEach(queryname => {
+        describe(`GET /${queryname}`, () => {
+            it(`should get all all ${queryname} records in the db.`, (done)=>{
+                chai.request(server)
+                    .get(`/${queryname}`)
+                    .end((err, response) => {
+                        response.should.have.status(200);
+                        response.body.should.be.a('array');
+                    done();
+                    })
+            })
+        })
+
+    })
+    
+
+
+    describe("GET /anwohner", () => {
+        it("should get all persons that live in a household.", (done)=>{
+            chai.request(server)
+                .get("/anwohner")
+                .query({haushalt_id: "1"})
+                .end((err, response) => {
+                    response.should.have.status(200);
+                    response.body.should.be.a('array');
+                done();
+                })
+        })
+    })
+
+
+    describe("GET /ag_mitglieder", () => {
+        it("should get all members of a workgroup.", (done)=>{
+            chai.request(server)
+                .get("/ag_mitglieder")
+                .query({arbeitsgruppe_id: "4"})
+                .end((err, response) => {
+                    response.should.have.status(200);
+                    response.body.should.be.a('array');
+                done();
+                })
+        })
+    })
+
+
+    describe("GET /dataMultitablePerson", () => {
+        it("should get all records of a Person in the db.", (done)=>{
+            chai.request(server)
+                .get("/dataMultitablePerson")
+                .query({table1: "kind_lerngruppe", table2:"lerngruppen", person_id: "280", sortByColumn: "eintrittsdatum"})
+                .end((err, response) => {
+                    response.should.have.status(200);
+                    response.body.should.be.a('array');
+                done();
+                })
+        })
+    })
+
+    // multitable queries tests
+
+    //dictionary containg parameter name as key and expected values as keyvalues
+    const multiQueries = {
+        haushalt_id: "2", 
+    }
+
+    const queryNames = ["dataMultitableHaus"]
+
+    for (var n in multiQueries){
+        let dictIndex = 0;
+        describe(`GET /${queryNames[dictIndex]}`, () => {
+            it("should get .", (done)=>{
+                chai.request(server)
+                    .get(`/${queryNames[dictIndex]}`)
+                    .query({haushalt_id: '2'})
+                    .end((err, response) => {
+                        response.should.have.status(200);
+                        response.body.should.be.a("array");
+                    done();
+                    })
+            })
+        })
+        dictIndex++;
+    }
+    
 })
